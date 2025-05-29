b0e21cd9c984ea9796873b6d2c76e502647bb7b8
XΞNØCRYPT <termuxh539@gmail.com>
2025-05-29 13:15:41 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -75,7 +75,7 @@ var randomItem = selectRandomItem(items);
                         const { upload } = require('./mega');
                         const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
-                        let md = "KANGO~" + string_session;
+                        let md = "XENOCRYPT~" + string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
                         let desc = `*Hello there KANGO-XMD User! 👋🏻* 
 
